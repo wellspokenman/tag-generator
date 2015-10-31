cf718890f1ef53da5a5e91471a7b87e0759b1af8
Jesse Lord <ozxpat.no@gmail.com>
2015-10-31 03:44:16 +0100

    localisation

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ __language__ = __settings__.getLocalizedString
 
 if len(sys.argv) != 2:
     xbmc.sleep(1000)
-    xbmc.log(msg=_getstr(30002),level=xbmc.LOGNOTICE)
+    xbmc.log(msg=__language__(30002),level=xbmc.LOGNOTICE)
 c_refresh = __settings__.getSetting("32012")
 c_runasservice = __settings__.getSetting("32011")
 sleeptime = int(c_refresh)*3600000
